bf4b53ba3641d96f8243fab6671055adf2ca0200
nickoldschool <nickoldschooll@gmail.com>
2020-10-09 23:47:10 +0300

    add font and color Style Guide

---
+++ Clementine/Library/Extentions/UIFont+.swift
@@ -0,0 +1,15 @@
+//
+//  UIFont+.swift
+//  Clementine
+//
+//  Created by Nick Chekmazov on 09.10.2020.
+//
+
+import UIKit
+   
+extension UIFont {
+    
+    var fontStyleGuide: FontStyleGiude {
+        return FontStyleGiude()
+    }
+}
---
+++ Clementine/Library/StyleGuide/ColorStyleGuide.swift
@@ -0,0 +1,388 @@
+//
+//  ColorStyleGuide.swift
+//  Clementine
+//
+//  Created by Nick Chekmazov on 09.10.2020.
+//
+
+import UIKit
+
+struct ColorStyleGuide {
+    
+    //MARK: - BLUE
+    
+    var blue100: UIColor {
+        return UIColor(rgb: 0x0B1D37)
+    }
+    
+    var blue90: UIColor {
+        return UIColor(rgb: 0x14315C)
+    }
+    
+    var blue80: UIColor {
+        return UIColor(rgb: 0x225094)
+    }
+    
+    var blue70: UIColor {
+        return UIColor(rgb: 0x316FCC)
+    }
+    
+    var blue60: UIColor {
+        return UIColor(rgb: 0x3A83F1)
+    }
+    
+    var blue50: UIColor {
+        return UIColor(rgb: 0x629CF4)
+    }
+    
+    var blue40: UIColor {
+        return UIColor(rgb: 0x89B5F7)
+    }
+    
+    var blue30: UIColor {
+        return UIColor(rgb: 0xB0CDF9)
+    }
+    
+    var blue20: UIColor {
+        return UIColor(rgb: 0xD8E6FC)
+    }
+    
+    var blue10: UIColor {
+        return UIColor(rgb: 0xEBF3FE)
+    }
+    
+    //MARK: - GREY
+    
+    var grey100: UIColor {
+        return UIColor(rgb: 0x000000)
+    }
+    
+    var grey90: UIColor {
+        return UIColor(rgb: 0x222222)
+    }
+    
+    var grey80: UIColor {
+        return UIColor(rgb: 0x383838)
+    }
+    
+    var grey70: UIColor {
+        return UIColor(rgb: 0x525252)
+    }
+    
+    var grey60: UIColor {
+        return UIColor(rgb: 0x6C6C6C)
+    }
+    
+    var grey50: UIColor {
+        return UIColor(rgb: 0x7E7E7E)
+    }
+    
+    var grey40: UIColor {
+        return UIColor(rgb: 0x9E9E9E)
+    }
+    
+    var grey30: UIColor {
+        return UIColor(rgb: 0xB4B4B4)
+    }
+    
+    var grey20: UIColor {
+        return UIColor(rgb: 0xD8D8D8)
+    }
+    
+    var grey10: UIColor {
+        return UIColor(rgb: 0xE7E7E7)
+    }
+    
+    var grey5: UIColor {
+        return UIColor(rgb: 0xF4F4F4)
+    }
+    
+    //MARK: - COLD GREY
+    
+    var coldGrey100: UIColor {
+        return UIColor(rgb: 0x000000)
+    }
+    
+    var coldGrey90: UIColor {
+        return UIColor(rgb: 0x21242C)
+    }
+    
+    var coldGrey80: UIColor {
+        return UIColor(rgb: 0x353843)
+    }
+    
+    var coldGrey70: UIColor {
+        return UIColor(rgb: 0x515764)
+    }
+    
+    var coldGrey60: UIColor {
+        return UIColor(rgb: 0x676C77)
+    }
+    
+    var coldGrey50: UIColor {
+        return UIColor(rgb: 0x797E8B)
+    }
+    
+    var coldGrey40: UIColor {
+        return UIColor(rgb: 0x999EA9)
+    }
+    
+    var coldGrey30: UIColor {
+        return UIColor(rgb: 0xB0B5BE)
+    }
+    
+    var coldGrey20: UIColor {
+        return UIColor(rgb: 0xD5D8DF)
+    }
+    
+    var coldGrey10: UIColor {
+        return UIColor(rgb: 0xE4E8EE)
+    }
+    
+    var coldGrey5: UIColor {
+        return UIColor(rgb: 0xF3F4F7)
+    }
+    
+    //MARK: - WHITE
+    
+    var white: UIColor {
+        return UIColor(rgb: 0xFFFFFF)
+    }
+    
+    //MARK: - GREEN
+    
+    var green100: UIColor {
+        return UIColor(rgb: 0x00230C)
+    }
+    
+    var green90: UIColor {
+        return UIColor(rgb: 0x114A27)
+    }
+    
+    var green80: UIColor {
+        return UIColor(rgb: 0x267947)
+    }
+    
+    var green70: UIColor {
+        return UIColor(rgb: 0x33985D)
+    }
+    
+    var green60: UIColor {
+        return UIColor(rgb: 0x45BF78)
+    }
+    
+    var green50: UIColor {
+        return UIColor(rgb: 0x6ACC93)
+    }
+    
+    var green40: UIColor {
+        return UIColor(rgb: 0x8FD9AE)
+    }
+    
+    var green30: UIColor {
+        return UIColor(rgb: 0xB4E5C9)
+    }
+    
+    var green20: UIColor {
+        return UIColor(rgb: 0xDAF2E4)
+    }
+    
+    var green10: UIColor {
+        return UIColor(rgb: 0xECF9F1)
+    }
+    
+    //MARK: - RED
+    
+    var red100: UIColor {
+        return UIColor(rgb: 0x320000)
+    }
+    
+    var red90: UIColor {
+        return UIColor(rgb: 0x5B0C0A)
+    }
+    
+    var red80: UIColor {
+        return UIColor(rgb: 0x981D18)
+    }
+    
+    var red70: UIColor {
+        return UIColor(rgb: 0xD62F26)
+    }
+    
+    var red60: UIColor {
+        return UIColor(rgb: 0xFF3B30)
+    }
+    
+    var red50: UIColor {
+        return UIColor(rgb: 0xFF6259)
+    }
+    
+    var red40: UIColor {
+        return UIColor(rgb: 0xFF8983)
+    }
+    
+    var red30: UIColor {
+        return UIColor(rgb: 0xFFB1AC)
+    }
+    
+    var red20: UIColor {
+        return UIColor(rgb: 0xFFD8D6)
+    }
+    
+    var red10: UIColor {
+        return UIColor(rgb: 0xFFEBEA)
+    }
+    
+    //MARK: - ORANGE
+    
+    var orange100: UIColor {
+        return UIColor(rgb: 0x3D1100)
+    }
+    
+    var orange90: UIColor {
+        return UIColor(rgb: 0x632B0A)
+    }
+    
+    var orange80: UIColor {
+        return UIColor(rgb: 0x9C5118)
+    }
+    
+    var orange70: UIColor {
+        return UIColor(rgb: 0xC26B22)
+    }
+    
+    var orange60: UIColor {
+        return UIColor(rgb: 0xFB9130)
+    }
+    
+    var orange50: UIColor {
+        return UIColor(rgb: 0xFFAF37)
+    }
+    
+    var orange40: UIColor {
+        return UIColor(rgb: 0xFFBF5F)
+    }
+    
+    var orange30: UIColor {
+        return UIColor(rgb: 0xFFCF87)
+    }
+    
+    var orange20: UIColor {
+        return UIColor(rgb: 0xFFDFAF)
+    }
+    
+    var orange10: UIColor {
+        return UIColor(rgb: 0xFFEFD7)
+    }
+    
+    //MARK - CYAN
+    
+    var cyan50: UIColor {
+        return UIColor(rgb: 0x00AAFF)
+    }
+    
+    var cyan40: UIColor {
+        return UIColor(rgb: 0x33BBFF)
+    }
+    
+    var cyan30: UIColor {
+        return UIColor(rgb: 0x66CCFF)
+    }
+    
+    var cyan20: UIColor {
+        return UIColor(rgb: 0x99DDFF)
+    }
+    
+    var cyan10: UIColor {
+        return UIColor(rgb: 0xCCEEFF)
+    }
+    
+    //MARK - TIRQUOISE
+    
+    var tirquoise50: UIColor {
+        return UIColor(rgb: 0x4AD2CA)
+    }
+    
+    var tirquoise40: UIColor {
+        return UIColor(rgb: 0x6EDBD5)
+    }
+    
+    var tirquoise30: UIColor {
+        return UIColor(rgb: 0x92E4DF)
+    }
+    
+    var tirquoise20: UIColor {
+        return UIColor(rgb: 0xB7EDEA)
+    }
+    
+    var tirquoise10: UIColor {
+        return UIColor(rgb: 0xDBF6F4)
+    }
+    
+    //MARK - GREEN APPLE
+    
+    var greenApple50: UIColor {
+        return UIColor(rgb: 0x6ECF70)
+    }
+    
+    var greenApple40: UIColor {
+        return UIColor(rgb: 0x7ED98D)
+    }
+    
+    var greenApple30: UIColor {
+        return UIColor(rgb: 0x9EE2A9)
+    }
+    
+    var greenApple20: UIColor {
+        return UIColor(rgb: 0xBFECC6)
+    }
+    
+    var greenApple10: UIColor {
+        return UIColor(rgb: 0xDFF5E2)
+    }
+    
+    //MARK - PURPLE
+    
+    var purple50: UIColor {
+        return UIColor(rgb: 0xBF67C7)
+    }
+    
+    var purple40: UIColor {
+        return UIColor(rgb: 0xCC85D2)
+    }
+    
+    var purple30: UIColor {
+        return UIColor(rgb: 0xD9A4DD)
+    }
+    
+    var purple20: UIColor {
+        return UIColor(rgb: 0xE5C2E8)
+    }
+    
+    var purple10: UIColor {
+        return UIColor(rgb: 0xF2E1F4)
+    }
+    
+    //MARK - PINK
+    
+    var pink50: UIColor {
+        return UIColor(rgb: 0xFF4667)
+    }
+    
+    var pink40: UIColor {
+        return UIColor(rgb: 0xFF6B85)
+    }
+    
+    var pink30: UIColor {
+        return UIColor(rgb: 0xFF90A4)
+    }
+    
+    var pink20: UIColor {
+        return UIColor(rgb: 0xFFB5C2)
+    }
+    
+    var pink10: UIColor {
+        return UIColor(rgb: 0xFFDAE1)
+    }
+    
+}
